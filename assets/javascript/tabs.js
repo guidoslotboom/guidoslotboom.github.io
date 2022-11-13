f0a42062fee3ed7b244c9dd860765e05faedc4e2
Guido Slotboom <development@guidoslotboom.nl>
2022-11-13 22:10:42 +0100

    Add initial tab snap styling
    Includes JavaScript file for the functional operation of the tab panel

---
+++ assets/javascript/tabs.js
@@ -0,0 +1,34 @@
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
+
+function handleTabClick(event) {
+
+    // hide all tab panels
+    tabPanels.forEach(panel => {
+        panel.hidden = true;
+    });
+    
+    // mark all tabs as unselected
+    tabButtons.forEach(tab => {
+        // tab.ariaSelected = false;
+        tab.setAttribute('aria-selected', false);
+    });
+    
+    // mark the clicked tab as selected
+    event.currentTarget.setAttribute('aria-selected', true);
+    
+    // find the associated tabPanel and show it!
+    const { id } = event.currentTarget;
+    
+    const tabPanel = tabPanels.find(
+        panel => panel.getAttribute('aria-labelledby') === id
+    );
+    tabPanel.hidden = false;
+    
+    // console.log(event);
+    // console.log(tabPanels);
+    // console.log(tabPanel);
+}
+
+tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
